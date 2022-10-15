17fc44ebfb8f25cb6d721f1b7ac406f01891c502
mariadefarges <mariadefarges@172.20.10.2>
2022-10-15 17:39:51 +0200

    First commit

---
+++ src/pojos/Patient.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pojos;
+
+/**
+ *
+ * @author mariadefarges
+ */
+public class Patient {
+    
+    private Integer patientId;
+    private String name;
+    private String surname;
+    private String gender;
+    private int heartrate;
+    
+    
+}
