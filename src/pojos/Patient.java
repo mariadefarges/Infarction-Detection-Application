8f1d6ff8717b96595deddd490662b5616881a314
mariadefarges <mariadefarges@172.20.10.2>
2022-10-18 16:15:15 +0200

    Bitalino library implemented and patient class changes done

--- src/pojos/Patient.java
+++ src/pojos/Patient.java
@@ -4,17 +4,78 @@
  */
 package pojos;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author mariadefarges
  */
-public class Patient {
+public class Patient implements Serializable {
     
-    private Integer patientId;
-    private String name;
-    private String surname;
-    private String gender;
-    private int heartrate;
+    private static final long serialVersionUID = -1L;
+    
+    private final Integer patientId;
+    private final String name;
+    private final String surname;
+    private final String gender;
+
+    public Patient(Integer patientId, String name, String surname, String gender) {
+        this.patientId = patientId;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }   
+      
+       @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.patientId);
+        hash = 89 * hash + Objects.hashCode(this.name);
+        hash = 89 * hash + Objects.hashCode(this.surname);
+        hash = 89 * hash + Objects.hashCode(this.gender);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        return Objects.equals(this.patientId, other.patientId);
+    }
     
     
 }
