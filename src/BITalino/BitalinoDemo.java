44a0bdf90b784d7f1991ef6925f4376d6549f945
carmenpozo <c.pozo8@usp.ceu.es>
2022-10-25 15:32:32 +0200

    Libreria

--- src/BITalino/BitalinoDemo.java
+++ src/BITalino/BitalinoDemo.java
@@ -1,10 +1,11 @@
 package BITalino;
 
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Vector;
 
 import javax.bluetooth.RemoteDevice;
-
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,28 +26,28 @@ public class BitalinoDemo {
 
             //You need TO CHANGE THE MAC ADDRESS
             //You should have the MAC ADDRESS in a sticker in the Bitalino
-            String macAddress = "20:16:02:14:75:76";
-            
+            String macAddress = "98:D3:51:FD:9C:72";
+
             //Sampling rate, should be 10, 100 or 1000
             int SamplingRate = 10;
             bitalino.open(macAddress, SamplingRate);
 
             // Start acquisition on analog channels A2 and A6
             // For example, If you want A1, A3 and A4 you should use {0,2,3}
-            int[] channelsToAcquire = {1, 5};
+            int[] channelsToAcquire = {1, 4};
             bitalino.start(channelsToAcquire);
 
             //Read in total 10000000 times
             for (int j = 0; j < 10000000; j++) {
 
                 //Each time read a block of 10 samples 
-                int block_size=10;
+                int block_size = 10;
                 frame = bitalino.read(block_size);
 
                 System.out.println("size block: " + frame.length);
 
                 //Print the samples
-                for (int i = 0; i < frame.length; i++) {
+                /*for (int i = 0; i < frame.length; i++) {
                     System.out.println((j * block_size + i) + " seq: " + frame[i].seq + " "
                             + frame[i].analog[0] + " "
                             + frame[i].analog[1] + " "
@@ -56,7 +57,42 @@ public class BitalinoDemo {
                     //  + frame[i].analog[5]
                     );
 
+                }*/
+ /* public static String guardarcomo(Experimento experimento) throws ExcepcionesExperimento {
+        String nombre;
+        System.out.println("Introduce el nombre del fichero que desea crear: ");
+        try {
+            nombre = console.readLine();
+
+            if (!nombre.endsWith(".txt")) {
+                nombre = nombre + ".txt";
+                guardar(experimento, nombre);
+            } else {
+                guardar(experimento, nombre);
+            }
+            return nombre;
+        } catch (IOException ex) {
+            Logger.getLogger(InputOutput.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }*/
+                    String nombre= null; // Pedir por pantalla
+                    PrintWriter fichero = new PrintWriter(new FileWriter(nombre), true);
+           
+                for (int i = 0; i < frame.length; i++) {
+                    System.out.println((j * block_size + i) + " seq: " + frame[i].seq + " "
+                            + frame[i].analog[0] + " "
+                            + frame[i].analog[1] + " "
+                            + frame[i].analog[2] + " "
+                 
+                    );
+
                 }
+
+                    fichero.println(experimento.getNpoblacion(i).toFile());
+
+                
+                fichero.close();
             }
             //stop acquisition
             bitalino.stop();
